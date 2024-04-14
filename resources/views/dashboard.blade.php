a43264891100edce02454b969d215a3ff5935049
dmackjaygee0321 <dmackjaygee0321@gmail.com>
2024-04-15 00:17:06 +0800

    Posting Done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -265,44 +265,53 @@
             }
 
             function postContent(post) {
-                return `<div class="col-lg-12">
+                let postType = `Announcement <svg style="margin-top: -10px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-megaphone-fill" viewBox="0 0 16 16">
+                                        <path d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0zm-1 .724c-2.067.95-4.539 1.481-7 1.656v6.237a25 25 0 0 1 1.088.085c2.053.204 4.038.668 5.912 1.56zm-8 7.841V4.934c-.68.027-1.399.043-2.008.053A2.02 2.02 0 0 0 0 7v2c0 1.106.896 1.996 1.994 2.009l.496.008a64 64 0 0 1 1.51.048m1.39 1.081q.428.032.85.078l.253 1.69a1 1 0 0 1-.983 1.187h-.548a1 1 0 0 1-.916-.599l-1.314-2.48a66 66 0 0 1 1.692.064q.491.026.966.06"/>
+                                    </svg> `
+
+                return `<div class="col-lg-12" style="padding-top: 10px">
     <div class="card">
  <div class="card-body">
-        <div class=" mt-3 mb-2">
-            <img src="/image/${post.admin.file}" style="width: 60px; height: 60px"
-                 class="img-thumbnail rounded-circle  mb-2">
-            <label class="mb-2">${post.admin.fName}</label>
-
+        <div class="mt-3 mb-2">
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="/image/${post.admin.file}" style="width: 60px; height: 60px"
+                         class="img-thumbnail rounded-circle  mb-2">
+                    <label class="mb-2">${post.admin.fName}</label>
+                </div>
+                <div class="col-lg-6" style="font-weight: bold; text-align: right">
+                        <div style="color: green">${postType}</div>
+                       ${new Date(post.created_at).toLocaleString()}
+                </div>
+            </div>
             <p class="card-text">${post.description}</p>
         </div>
 
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-12"  style="margin-bottom: 10px">
                 ${postImage(post)}
             </div>
-
-            <div class="col-lg-12" style="padding-top: 10px">
-                ${postComment(post.comments)}
-
-
-
-                <div class="col-lg-12 row">
-                    <div class="col-lg-11">
-                        <textarea type="text" class="input" placeholder="Write a comment" rows="5" id="description${post.id}"></textarea>
-                    </div>
-                    <div class="col-lg-1">
-                        <button class='primaryContained mb-10 comment' style="width: 100%" data-id="${post.id}">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="16" fill="currentColor"
-                                 class="bi bi-send-arrow-up-fill" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd"
-                                      d="M15.854.146a.5.5 0 0 1 .11.54L13.026 8.03A4.5 4.5 0 0 0 8 12.5c0 .5 0 1.5-.773.36l-1.59-2.498L.644 7.184l-.002-.001-.41-.261a.5.5 0 0 1 .083-.886l.452-.18.001-.001L15.314.035a.5.5 0 0 1 .54.111M6.637 10.07l7.494-7.494.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154z"/>
-                                <path fill-rule="evenodd"
-                                      d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354a.5.5 0 0 0-.722.016l-1.149 1.25a.5.5 0 1 0 .737.676l.28-.305V14a.5.5 0 0 0 1 0v-1.793l.396.397a.5.5 0 0 0 .708-.708z"/>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
+            <div class="col-lg-12"  style="margin-bottom: 10px">
+                <div style="border-top: 1px solid #23ffd3; margin-top: 10px"></div>
             </div>
+
+            <div class="col-lg-12" style="overflow-y: scroll; max-height:400px; padding-top: 10px;">
+                ${postComment(post.comments)}
+            </div>
+            <div class="col-lg-12" style="padding-top: 20px">
+                        <textarea type="text" class="input" placeholder="Write a comment" rows="3" id="description${post.id}"></textarea>
+                        <div style="float: right">
+                            <button class='primaryContained comment' style="width: 150px;" data-id="${post.id}">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="16" fill="currentColor"
+                                     class="bi bi-send-arrow-up-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                          d="M15.854.146a.5.5 0 0 1 .11.54L13.026 8.03A4.5 4.5 0 0 0 8 12.5c0 .5 0 1.5-.773.36l-1.59-2.498L.644 7.184l-.002-.001-.41-.261a.5.5 0 0 1 .083-.886l.452-.18.001-.001L15.314.035a.5.5 0 0 1 .54.111M6.637 10.07l7.494-7.494.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154z"/>
+                                    <path fill-rule="evenodd"
+                                          d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354a.5.5 0 0 0-.722.016l-1.149 1.25a.5.5 0 1 0 .737.676l.28-.305V14a.5.5 0 0 0 1 0v-1.793l.396.397a.5.5 0 0 0 .708-.708z"/>
+                                </svg>
+                            </button>
+                        </div>
+                </div>
         </div>
 
     </div>
@@ -315,12 +324,17 @@
                 let display = "";
 
                 for (const comment of comments) {
-                    display += `<div class="col-lg-12 mb-2 row">
-                    <div class="col-lg-3 row">
+                    display += `<div class="col-lg-12 mb-2 row" style="border:#23ffd3 1px solid; padding: 15px; margin-left: 6px">
+                    <div class="col-lg-6">
                             <img src="/image/${comment.admin.file}" style="width: 60px; height: 60px" class="img-thumbnail rounded-circle  mb-2">
                             <label class="mb-2">${comment.admin.fName}</label>
                     </div>
-                    <div class="col-lg-9" style="border:gray 1px solid;1">
+                    <div class="col-lg-6" style="text-align: right; font-weight: bold">
+                            ${new Date(comment.created_at).toLocaleString()}
+                    </div>
+                    <div class="col-lg-4">
+                    </div>
+                    <div class="col-lg-8" style="border:#23ffd3 1px solid; padding: 15px">
                             ${comment.description}
                     </div>
                 </div>`
@@ -348,6 +362,17 @@
                     index++;
                 }
 
+                let controls = "";
+                if(images.length > 1)
+                    controls = `<a class="left carousel-control" href="#myCarousel${post.id}" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel${post.id}" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right"></span>
+                          <span class="sr-only">Next</span>
+                        </a>`;
+
                 return `
                       <div id="myCarousel${post.id}" class="carousel slide" data-ride="carousel" >
                         <!-- Indicators -->
@@ -361,14 +386,7 @@
                         </div>
 
                         <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel${post.id}" data-slide="prev">
-                          <span class="glyphicon glyphicon-chevron-left"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel${post.id}" data-slide="next">
-                          <span class="glyphicon glyphicon-chevron-right"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
+                        ${controls}
                       </div>`
             }
 
