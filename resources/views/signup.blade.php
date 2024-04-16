609055f3d5ab87b5b646f2f77e516f042c2513ee
jansenxix <jungwonie72@gmail.com>
2024-04-16 17:11:34 +0800

    Final login form

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -6,32 +6,67 @@
     <title>Admin Login</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-</head>
-<body>
+    <link rel="stylesheet" href="login.css">
 
- <div class="class" id="test">
-     <h1>Admin login</h1>
+    <!-- *Remixicon -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+<body style="background-image: url('css/background.jpeg">
+
+
+     
         <form action="POST" action="" id="signup">
             @csrf
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Username:</label>
-                <input type="username" class="form-control" id="username" placeholder="Enter Username">
-              </div>
+            <div class="container">
+            <div class="design">
+                <img src="css/logo.png" id="img"   style="margin-left:10%; margin-top:10%; height:700; width:350px; object-fit:scale-down; object-position:center center;   border-radius:100%;" >
+           </div>
 
-              <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Password:</label>
-                <input type="password" class="form-control" id="password" placeholder="Enter Password">
-              </div>
-              
-              <button type="submit" id="btn" class="btn btn-danger ml-40" >Submit</button>
+        <!-- *LOGIN FORM -->
+             <div class="login mb-5">
+                  <h1 class="title" style="color:white;">Login Now!</h1>
+             <div class="mb-3" id="inputTypetest">
+
+                <div class="text-input">
+                    <i class="ri-user-fill"></i>
+                    <input type="text" class="form-control" id="username" placeholder="Enter Username" size="50px">
+                </div>
+
+
+
+                <div class="text-input" style="margin-bottom: 20px;"> 
+                    <i class="ri-lock-fill"></i>
+                    <input type="password" id="password" placeholder="Enter Password">
+                            
+                    <div class="eye">
+                            <span class="Eye" onclick="myFunction()">
+                            <i id="hide1" style="display: none" class="ri-eye-line"></i>
+                           <i id="hide2" class="ri-eye-off-line"></i>
+                    </div>
+                </div>
+                <button type="submit" class="login-btn"id="btn">LOGIN</button>
+        
+        <div class="create" style="color:white; ">
+            Don't have an account? <a href="../signup/signup.php"> &nbsp; <u>Sign up now!</u></a>
         </div>
-    </div>
-    
-</body>
-</html>
+        <div>
+            <a href="#" class="forgot" style="color:white;">Forgot password?</a>
+        </div>
 
+   
+
+ 
+
+    
+    <div class="create" style="color:white; ">
+        Don't have an account? <a href="/userSignup"> &nbsp; <u>Sign up now!</u></a>
+    </div>
+</div> <!-- *END CONTAINER -->
+
+
+<script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script>
  $(document).ready(function () {
@@ -60,4 +95,26 @@
             });
         });
     });
+
+
+       
+    function myFunction()
+{
+        var x = document.getElementById("password");
+        var y = document.getElementById("hide1");
+        var z = document.getElementById("hide2");
+
+        if(x.type === 'password'){
+            x.type = "text";
+            y.style.display = "block";
+            z.style.display = "none";
+        }
+        else{
+            x.type = "password";
+            y.style.display = "none";
+            z.style.display = "block";
+        }
+    }
 </script>
+</body>
+</html>
--- resources/views/userSignup.blade.php
+++ resources/views/userSignup.blade.php
@@ -7,41 +7,62 @@
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <title>User sign up!</title>
     <link rel="stylesheet" href="{{ asset('css/userSignup.css') }}">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
-<body style="display: flex; justify-content: center;">
-    <div class="class" id="test">
-        <h1>User sign-up</h1>
-           <form action="POST" action="" id="userSignup">
-               @csrf
-               <div class="mb-3">
-                   <label for="exampleFormControlInput1" class="form-label">Firstname:</label>
-                   <input type="text" class="form-control" id="fname" name="fname" placeholder="Enter Firstname:" required>
-                 </div>
+<body style="background-image: url('css/background.jpeg">
+
+          <form action="POST" action="" id="userSignup">
+            @csrf
+            <div class="container">
+              <div class="design">
+                  <img src="css/logo.png" id="img"   style="margin-left:10%; margin-top:10%; height:700; width:350px; object-fit:scale-down; object-position:center center;   border-radius:100%;" >
+              </div>
+
+              <div class="login mb-5">
+                    <h1 class="title" style="color:white;"> Sign-Up Now!</h1>
+              <div class="mb-3" id="inputTypetest">
+
+
+              <div class="text-input">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                </svg>
+                <input type="text" class="form-control"  id="fname" name="fname" placeholder="Enter First Name:" required size="50 px">
+              </div>
+               
+              
+              <div class="text-input" style="margin-bottom: 5px;"> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                </svg>
+                <input type="text" id="lname" name="lname" placeholder="Enter Last Name">
+              </div>
+
+              <div class="text-input" style="margin-bottom: 20px;"> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                </svg>
+                <input type="text" id="snumber" name="snumber" placeholder="Enter Student Number:">
+              </div>
+
+
+               <button type="submit" id="btn" class="btn btn-primary login-btn">Submit</button>
+               <div class="create" style="color:white;">
+                Have an account already?&nbsp; <a href="/logout">Login now!</a>
+                </div>
+
+              
+        </div>
+      </div>
+    </div> <!-- *END CONTAINER -->
    
-                 <div class="mb-3">
-                   <label for="exampleFormControlInput1" class="form-label">Lastname:</label>
-                   <input type="text" class="form-control" id="lname" name="lname" placeholder="Enter Lastname" required>
-                 </div>
 
-                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Student number:</label>
-                    <input type="number" class="form-control" id="snumber" name="snumber" placeholder="Enter student number:">
-                  </div>
-                 
-                 <button type="submit" id="btn" class="btn btn-danger ml-40" >Submit</button>
+{{-- test --}}
 
-                 <div class="already">
-                  Have an account already?&nbsp; <a href="">Login now!</a>
-                  </div>
-           </div>
-       </div>
-       
-</body>
-</html>
-
+{{-- test --}}
+<script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script>
 
@@ -79,3 +100,5 @@
     });
 });
 </script>
+</body>
+</html>
